94bf81ddfa384c5c4586b423547082999d2c92e0
annabush092 <annabush092@gmail.com>
2017-08-29 17:01:31 -0400

    Revised and bettergit add .

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,46 +1,67 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require_relative "../lib/command_line_interface.rb"
 
+#returns a nested hash from the website api. If no website provided, the default is /people page 1.
 def request_and_parse(url = 'http://www.swapi.co/api/people/')
   all_characters = RestClient.get(url)
   JSON.parse(all_characters)
 end
 
-def get_character_movies_from_api(character)
-  #make the web request and find character's hash we've searched for
-  character_data = get_character_hash(character) #returns character hash of the character we've searched for
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #   for that film
-  # return value of this method should be collection of info about each film.
-  #   i.e. an array of hashes in which each hash reps a given film
-  collect_film_info(character_data)
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-end
-
+#takes a character name and finds and returns the hash for that character from the api
 def get_character_hash(character)
   character_hash = request_and_parse
   my_character_hash = nil
   while !my_character_hash do
     my_character_hash = character_hash["results"].find do |my_char_hash|
-      my_char_hash["name"] == character
+      my_char_hash["name"].downcase == character
     end
-    character_hash = request_and_parse(character_hash["next"])
+    #if the character name does not show up on the current page, check the next pages.
+    #if the character name does not appear on any pages (there is no "next"), report the issue.
+    unless character_hash["next"]==nil
+      character_hash = request_and_parse(character_hash["next"])
+    else
+      puts "Sorry, there is no character #{character}."
+      return {}
+    end
   end
   my_character_hash
 end
 
+#takes a hash of a specific character's data and returns an array of hashes
+#each hash contains data for a film
 def collect_film_info(character_data)
-  character_data["films"].map do |film_url|
-    request_and_parse(film_url)
-  end
+    character_data["films"].map do |film_url|
+      request_and_parse(film_url)
+    end
 end
 
+#if character is not found in any of the pages, request a new character from user
+#use the new name they input to find the hash of that character, and return it
+def get_new_character
+  character = get_character_from_user
+  get_character_hash(character)
+end
+
+#takes a character name and searches for it in the Star Wars api
+#within the character's hash, find the films they appear in
+#use the film api's to make an array of film hashes, and return this array
+def get_character_movies_from_api(character)
+  #find character's hash we've searched for
+  character_data = get_character_hash(character)
+  #if character is not found in any of the pages, request a new character from user
+  while character_data == {}
+    character_data = get_new_character
+  end
+  # iterate over the character hash to find the collection of `films` for the given
+  #   `character`
+  # collect those film API urls, make a web request to each URL to get the info
+  #   for that film, and put the hasehs of film info into an array.
+  collect_film_info(character_data)
+end
+
+#takes an array of film hashes and puts out the titles from the nested film data.
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
   films_hash.each do |film|
@@ -48,8 +69,11 @@ def parse_character_movies(films_hash)
   end
 end
 
+#connects the get_character_movies_from_api and parse_character_movies methods
 def show_character_movies(character)
+  #obtains an array of film hashes from get_character_movies_from_api
   films_hash = get_character_movies_from_api(character)
+  #and then puts that array of film hashes into parse_character_movies
   parse_character_movies(films_hash)
 end
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,7 +4,7 @@ def welcome
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  gets.chomp
+  puts "Please enter a character. If you'd like to exit, type 'exit'."
+  response = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
